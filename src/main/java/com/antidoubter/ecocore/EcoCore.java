256c085f089928ebeea64ccc7c79495a187f2f56
RobberChieftain <127794005+RobberChieftain@users.noreply.github.com>
2023-03-13 15:45:17 -0700

    Setup main class and plugin.yml

---
+++ src/main/java/com/antidoubter/ecocore/EcoCore.java
@@ -0,0 +1,20 @@
+package com.antidoubter.ecocore;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EcoCore extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this,this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
+}
